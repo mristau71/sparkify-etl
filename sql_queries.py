890035b739f27858b3054eacf338e812a888a725
Mike Ristau <mike.ristau@nielsen.com>
2020-03-29 18:32:52 -0400

    update SQL queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,67 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+  songplay_id int,
+  start_time timestamp,
+  user_id int,
+  level varchar,
+  song_id varchar,
+  artist_id varchar,
+  session_id int,
+  location varchar,
+  user_agent varchar
+);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+  user_id int,
+  first_name varchar,
+  last_name varchar,
+  gender char(1),
+  level varchar
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+  song_id varchar,
+  title varchar,
+  artist_id varchar,
+  year int,
+  duration numeric
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+  artist_id varchar,
+  name varchar,
+  location varchar,
+  latitude numeric,
+  longitude numeric
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+  start_time timestamp,
+  hour int,
+  day int,
+  week int,
+  month int,
+  year int,
+  weekday varchar
+);
 """)
 
 # INSERT RECORDS
@@ -32,6 +73,8 @@ user_table_insert = ("""
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, year, duration)
+VALUES (%s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
